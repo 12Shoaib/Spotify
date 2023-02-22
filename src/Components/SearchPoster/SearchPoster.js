600d5a3a9e36b34cdfd73f80639cc922a31778aa
shoaib <shoaibmohd916@gmail.com>
2023-02-23 02:07:35 +0530

    Live song play functionality is sucessfully added

--- src/Components/SearchPoster/SearchPoster.js
+++ src/Components/SearchPoster/SearchPoster.js
@@ -2,10 +2,11 @@ import searchposter from './searchposter.module.css'
 import {FiSearch} from 'react-icons/fi'
 import { useEffect , useState } from 'react'
 
-const data = [1,1,2,1,1,1,1,,11,1]
+const data = [1,2,3,4,5,6,7,8,9,0]
 const SearchPoster = () => {
     const [albums , setAlbums] = useState([])
     const [loader , setLoader] = useState(false)
+    const [music , setMusic] = useState('')
 
     // useEffect(()=> {
         const options = {
@@ -17,12 +18,18 @@ const SearchPoster = () => {
         };
         const fetchAlbums = async() => {
             setLoader(true)
-            const response = await fetch('https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0', options)
+            const response = await fetch('https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=20', options)
             const data  = await response.json()
             setAlbums(data.tracks)
             setLoader(false)
         }
-    // },[])
+        // },[])
+        localStorage.setItem("data" , JSON.stringify(albums))
+        const selectedAlbumSong =   JSON.parse(localStorage.getItem('data'))
+        
+    function ClickedValue (index)    {
+        setMusic(selectedAlbumSong[index].hub.actions[1].uri)
+    }
 
     return (
         <div className={searchposter.main__component}>
@@ -30,11 +37,12 @@ const SearchPoster = () => {
              <FiSearch className={searchposter.search__Icon} />
               <input placeholder='What do you want to listen to?' className={searchposter.search__Bar}  />
               <p onClick={fetchAlbums}>Click HERE TO FETCH</p>
+              
             </div>
             
            {loader && <div className={searchposter.loader__card__Container}>
 
-            {data?.map((element) =><div className={searchposter.loader__card__Wrapper}>
+            {data?.map(() =><div className={searchposter.loader__card__Wrapper}>
                
                <p className={searchposter.loader__searchposter__Img} ></p>
                {/* <h6>{element.share.subject}</h6> */}
@@ -45,15 +53,14 @@ const SearchPoster = () => {
             </div>}
 
                 <div className={searchposter.card__Container}>
-
-                    { albums?.map((element) =><div className={searchposter.card__Wrapper}>
+                <audio src={music}  controls/>
+                    { albums?.map((element , index) =><div className={searchposter.card__Wrapper}>
 
                     {/* <img className={searchposter.searchposter__Img} src='https://i.scdn.co/image/ab67616d00001e02c08202c50371e234d20caf62' /> */}
-                    <img className={searchposter.searchposter__Img} src={element.share.image} />
+                    <img onClick={()=>ClickedValue(index)} className={searchposter.searchposter__Img} src={element.share.image} />
                     <h6>{element.share.subject}</h6>
                     {/* <h2>Heloo EveryOne..!!</h2> */}
                     </div>)}
-
                 </div>
 
 
