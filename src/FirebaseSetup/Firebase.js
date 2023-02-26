684e23719c8360a0c54b7003f86baad380f6ac0e
shoaib <shoaibmohd916@gmail.com>
2023-02-27 03:11:52 +0530

    Initializing firebase

---
+++ src/FirebaseSetup/Firebase.js
@@ -0,0 +1,14 @@
+import { getAuth } from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5GZpzlydKdPdUHZX9GpOPQha3V5AVCxM",
+  authDomain: "spotify-clone-aba28.firebaseapp.com",
+  projectId: "spotify-clone-aba28",
+  storageBucket: "spotify-clone-aba28.appspot.com",
+  messagingSenderId: "1012432128818",
+  appId: "1:1012432128818:web:824ce94eb1ced7d7e8e52b"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
