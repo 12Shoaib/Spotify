d8bfb19990592c977ac97955f5bd0ce6db24c19b
shoaib <shoaibmohd916@gmail.com>
2023-02-28 18:48:09 +0530

    Signout

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,28 @@
 import header from './header.module.css'
 import {AiOutlineCaretDown} from 'react-icons/ai'
 import {SlUser} from 'react-icons/sl'
+import {useState} from 'react'
+import {loginValue} from '../../Recoil/Recoil'
+import { useSetRecoilState } from 'recoil'
 
 const Header = () => {  
+    const [show , setShow] = useState(false)
+    const isLogOut = useSetRecoilState(loginValue)
+    const handleShow = () => {
+        setShow(!show)
+    }
+    const logOut = () => {
+        isLogOut(false)
+    }
 
     return (
         <div className={header.main__Component}>
             <p className={header.sub__Heading}>Explore Premium</p>
-            <p className={header.sub__Heading2} ><span className={header.profilIcon__Wrapper}><SlUser /></span>
+            <p onClick={handleShow} className={header.sub__Heading2} ><span className={header.profilIcon__Wrapper}><SlUser /></span>
              user_123 <AiOutlineCaretDown /></p>
+           {show &&  <div onClick={logOut} className={header.show_Component}>
+                Log Out 
+             </div>}
         </div>
     )
 }
